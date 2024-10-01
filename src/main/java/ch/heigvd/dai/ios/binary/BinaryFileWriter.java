47b6fe22b9de7451005005c5c3cd2f544a12e7e3
Syseria <nicocarbo@live.fr>
2024-10-01 14:21:39 +0200

    Implementation of BinaryFileWriter.

--- src/main/java/ch/heigvd/dai/ios/binary/BinaryFileWriter.java
+++ src/main/java/ch/heigvd/dai/ios/binary/BinaryFileWriter.java
@@ -2,6 +2,10 @@ package ch.heigvd.dai.ios.binary;
 
 import ch.heigvd.dai.ios.Writable;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 /**
  * A class that writes binary files. This implementation writes the file byte per byte. It manages
  * the file output stream properly with a try-catch-finally block.
@@ -10,6 +14,12 @@ public class BinaryFileWriter implements Writable {
 
   @Override
   public void write(String filename, int sizeInBytes) {
-
+    try (OutputStream os = new FileOutputStream(filename)) {
+      for (int i = 0; i < sizeInBytes; ++i) {
+        os.write(i);
+      }
+    } catch (IOException e) {
+      System.out.println("Exception: " + e);
+    }
   }
 }
