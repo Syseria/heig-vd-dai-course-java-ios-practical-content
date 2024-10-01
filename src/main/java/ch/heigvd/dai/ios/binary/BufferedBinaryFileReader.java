43204660639b3936687796b76e31860652031e23
Syseria <nicocarbo@live.fr>
2024-10-01 13:13:18 +0200

    Implemented the non-buffered version of BinaryFileReader. Correction of BinaryFileReader which was already buffering... Swapping the 2 methods.

--- src/main/java/ch/heigvd/dai/ios/binary/BufferedBinaryFileReader.java
+++ src/main/java/ch/heigvd/dai/ios/binary/BufferedBinaryFileReader.java
@@ -2,6 +2,9 @@ package ch.heigvd.dai.ios.binary;
 
 import ch.heigvd.dai.ios.Readable;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
 /**
  * A class that reads binary files. This implementation reads the file using a buffered input
  * stream. It manages the input stream and the buffered input stream properly with a
@@ -11,6 +14,15 @@ public class BufferedBinaryFileReader implements Readable {
 
   @Override
   public void read(String filename) {
-    throw new UnsupportedOperationException("Please remove this exception and implement this method.");
+    try (InputStream is = new FileInputStream(filename);
+         Reader reader = new InputStreamReader(is, StandardCharsets.UTF_8);
+         BufferedReader br = new BufferedReader(reader)) {
+      int c;
+      while ((c = br.read()) != -1) {
+        System.out.print(c);
+      }
+    } catch (IOException e) {
+      System.out.println("Exception: " + e);
+    }
   }
 }
