4de94b2ca53bf49c4ae8413ab640a21cd9df509d
Syseria <nicocarbo@live.fr>
2024-10-01 11:36:20 +0200

    Implemented BinaryFileReader, throws error

--- src/main/java/ch/heigvd/dai/ios/binary/BinaryFileReader.java
+++ src/main/java/ch/heigvd/dai/ios/binary/BinaryFileReader.java
@@ -2,6 +2,9 @@ package ch.heigvd.dai.ios.binary;
 
 import ch.heigvd.dai.ios.Readable;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
 /**
  * A class that reads binary files. This implementation reads the file byte per byte. It manages the
  * file input stream properly with a try-catch-finally block.
@@ -10,6 +13,16 @@ public class BinaryFileReader implements Readable {
 
   @Override
   public void read(String filename) {
-    throw new UnsupportedOperationException("Please remove this exception and implement this method.");
+    // throw new UnsupportedOperationException("Please remove this exception and implement this method.");
+    try (InputStream is = new FileInputStream(filename);
+         Reader reader = new InputStreamReader(is, StandardCharsets.UTF_8);
+         BufferedReader br = new BufferedReader(reader)) {
+      int c;
+      while ((c = br.read()) != -1) {
+        System.out.print(c);
+      }
+    } catch (IOException e) {
+      System.out.println("Exception: " + e);
+    }
   }
 }
