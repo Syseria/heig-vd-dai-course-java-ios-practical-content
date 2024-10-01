4fe097815b8fa3af80c5886e9569d601b403b939
Syseria <nicocarbo@live.fr>
2024-10-01 14:00:26 +0200

    I implemented the text versions instead of the binary ones. Correcting and placing them in the textFile files.

--- src/main/java/ch/heigvd/dai/ios/binary/BufferedBinaryFileWriter.java
+++ src/main/java/ch/heigvd/dai/ios/binary/BufferedBinaryFileWriter.java
@@ -2,9 +2,6 @@ package ch.heigvd.dai.ios.binary;
 
 import ch.heigvd.dai.ios.Writable;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-
 /**
  * A class that writes binary files. This implementation writes the file using a buffered output
  * stream. It manages the file output stream properly with a try-with-resources block.
@@ -13,14 +10,6 @@ public class BufferedBinaryFileWriter implements Writable {
 
   @Override
   public void write(String filename, int sizeInBytes) {
-    try (OutputStream os = new FileOutputStream(filename);
-         Writer writer = new OutputStreamWriter(os, StandardCharsets.UTF_8);
-         BufferedWriter bw = new BufferedWriter(writer)) {
-      for (int i = 0; i < sizeInBytes; ++i) {
-        bw.write(i);
-      }
-    } catch (IOException e) {
-      System.out.println("Exception: " + e);
-    }
+
   }
 }
